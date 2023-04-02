b5ac2a6aa1391f12bc2fe6cdb496102b01a077af
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2023-04-02 16:38:13 -0400

    Configure CORS and modify account API

--- backend/easychef/account/models.py
+++ backend/easychef/account/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures',
                                         null=True, blank=True)
     favourite_recipes = models.ManyToManyField('recipes.Recipe',
                                                related_name='favourited_by')
+    email = models.EmailField(unique=True)
