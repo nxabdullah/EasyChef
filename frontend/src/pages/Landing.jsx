e172a7d1960473f850cf0d89e16adc08e8e5f3a7
moonscape09 <munim.adil@outlook.com>
2023-04-11 18:01:51 -0400

    pagination

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,4 +1,5 @@
-//REMEMBER TO PUSH TO
+//POPULAR RECIPE ENDPOINT
+//RECIPE CARDS DISPLAY
 
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -13,27 +14,29 @@ function Landing() {
   const [cuisines, setCuisines] = useState([]);
   const [diets, setDiets] = useState([]);
   const [cookTime, setCookTime] = useState([0, 121]); // default min and max cook times
+  const [page, setPage] = useState(1)
 
-  //useEffect hook for popular recipes (no params)
-  useEffect(() => {
-    const fetchPopularRecipes = async () => {
-      try {
-        const response = await axios.get(SEARCH_ENDPOINT);
-        setPopularRecipes(response.data.results);
-      } catch (error) {
-        console.error("Error fetching popular recipes:", error);
-      }
-    };
+  // //useEffect hook for popular recipes (no params)
+  // useEffect(() => {
+  //   const fetchPopularRecipes = async () => {
+  //     try {
+  //       const response = await axios.get(SEARCH_ENDPOINT, { page });
+  //       setPopularRecipes(response.data.results);
+  //     } catch (error) {
+  //       console.error("Error fetching popular recipes:", error);
+  //     }
+  //   };
 
-    fetchPopularRecipes();
-  }, []);
-
+  //   fetchPopularRecipes();
+  // }, [page]);
+  
   //useEffect hook for queried recipes (uses same hook function: setPopularRecipes)
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
         const params = {
           //params available
+          page: page,
           search: searchQuery,
           cuisines: cuisines.join(","),
           diets: diets.join(","),
@@ -42,21 +45,31 @@ function Landing() {
         };
 
         const response = await axios.get(SEARCH_ENDPOINT, { params }); //endpoint plus params
-        console.log(params);
-        setPopularRecipes(response.data.results); //produce results (PAGINATION YET TO BE ADDRESSED)
+        
+        const newRecipes = response.data.results
+        if (page === 1){
+          setPopularRecipes(newRecipes); //produce results (PAGINATION YET TO BE ADDRESSED)
+        } else {
+            // Otherwise, concatenate the new recipes with the previous ones
+            setPopularRecipes((prevRecipes) => [...prevRecipes, ...newRecipes]);
+        }
       } catch (error) {
         console.error("Error fetching recipes:", error);
       }
     };
 
     fetchRecipes();
-  }, [searchQuery, cuisines, diets, cookTime]); //dependency array for the hook (when any of these change, the hook runs again)
+  }, [searchQuery, cuisines, diets, cookTime, page]); //dependency array for the hook (when any of these change, the hook runs again)
 
   //event handlers to update respective state variables upon user itneraction
   const handleSearchChange = (event) => {
+
     setSearchQuery(event.target.value);
   };
 
+  const handleShowMore = () => {
+    setPage(prevPage => prevPage + 1);
+  }
   const handleCuisinesChange = (selectedOptions) => {
     setCuisines(selectedOptions.map((option) => option.value));
   };
@@ -68,8 +81,6 @@ function Landing() {
     setCookTime(newValue);
 
   };
-  console.log(popularRecipes);
-
   return (
     <div>
       <Search
@@ -86,7 +97,7 @@ function Landing() {
         {searchQuery ? "Search Results" : "Popular on Easychef"}
       </h3>
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        {popularRecipes.map((recipe, index) => (
+        {popularRecipes.map((recipe) => (
           <div key={recipe.id} className="col">
             <RecipeCard
               id={recipe.id}
@@ -98,6 +109,9 @@ function Landing() {
             />
           </div>
         ))}
+        {popularRecipes.length >= 0 && (
+           <button onClick={handleShowMore}>Show More</button>
+           )}
       </div>
     </div>
   );
