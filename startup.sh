6b9ab0ec02641098541abbe11032e6acbccc5257
moonscape09 <munim.adil@outlook.com>
2023-04-13 18:43:04 -0400

    Setup scripts

---
+++ startup.sh
@@ -0,0 +1,16 @@
+#create virtual environment
+python3 -m venv venv
+
+#activate virtual environment
+source $(pwd)/venv/bin/activate
+
+#install backend dependencies
+python3 -m pip install -r backend/requirements.txt
+
+#set backend migration
+python3 easychef/manage.py migrate
+
+#install frontend dependencies
+npm --prefix frontend install
+
+echo "Startup complete"
